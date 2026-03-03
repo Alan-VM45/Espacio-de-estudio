efb898a6ec702c52181541d9544f6ec2c7183f14
Alan-VM45 <113520689+Alan-VM45@users.noreply.github.com>
2026-03-03 00:47:27 -0300

    hkoliijn
    gjhijj

---
+++ config.js
@@ -0,0 +1,7 @@
+// CONFIGURACIÓN DE PERSISTENCIA
+// Este archivo guarda tu clave automáticamente en el navegador.
+
+localStorage.setItem('gemini_api_key', "AIzaSyCU9Wh5I7tXS-mK6PYW6HHDimMSvc-Z1kI");
+localStorage.setItem('gemini_model', "gemini-2.5-flash");
+
+console.log("Configuración inyectada desde config.js correctamente.");
